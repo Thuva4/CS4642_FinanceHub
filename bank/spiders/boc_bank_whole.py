57d5af9ae9d5ff53d54e9d682953bbdf67500542
thuva4 <tharma.thuva@gmail.com>
2018-07-04 21:01:15 +0530

    Spider to crawl oc, commercial and peoples bank
    Signed-off-by: thuva4 <tharma.thuva@gmail.com>

---
+++ bank/spiders/boc_bank_whole.py
@@ -0,0 +1,54 @@
+import json
+
+import scrapy
+from scrapy.linkextractor import LinkExtractor
+from scrapy.spiders import Rule, CrawlSpider
+# from datablogger_scraper.items import DatabloggerScraperItem
+
+
+class DatabloggerSpider(CrawlSpider):
+    # The name of the spider
+    name = "bocwhole"
+    object_url = {}
+    page = 0
+
+    # The domains that are allowed (links to other domains are skipped)
+    allowed_domains = ["web.boc.lk"]
+
+    # The URLs to start with
+    start_urls = ["http://web.boc.lk/boc/index.php"]
+
+    # This spider has one rule: extract all (unique and canonicalized) links, follow them and parse them using the parse_items method
+    rules = [
+        Rule(
+            LinkExtractor(
+                canonicalize=True,
+                unique=True
+            ),
+            follow=True,
+            callback="parse_item"
+        )
+    ]
+
+    # Method which starts the requests by visiting all URLs specified in start_urls
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url, callback=self.parse, dont_filter=True)
+
+
+    # Method for parsing items
+    def parse_item(self, response):
+        self.page += 1
+        # filename = './bank/data/' + str(self.page) + '.json'
+        filename = './bank/data/boc/html/' + str(self.page) + '.html'
+        with open(filename, 'wb') as f:
+            # object = {
+            #     'url': response.url
+            # }
+            # json.dump(object, f)
+            self.object_url[self.page] = response.url
+
+            f.write(response.body)
+        filename_json = './bank/data/boc/url_map.json'
+        with open(filename_json, 'w') as f:
+            json.dump(self.object_url, f)
